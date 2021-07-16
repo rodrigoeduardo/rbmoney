0c3c18cd6ef59c2006941002eb53198fe764b69b
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-07-15 23:28:18 -0300

    finishing project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Modal from 'react-modal';
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { GlobalStyle } from "./styles/global";
+import { useState } from 'react';
+import { NtModal } from './components/NTModal';
 
-function App() {
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  function handleOpenModal() {
+    setModalOpen(true);
+  }
+
+  function handleCloseModal() {
+    setModalOpen(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header onOpenModal={handleOpenModal} />
+      <Dashboard />
+
+      <NtModal isOpen={isModalOpen} onRequestClose={handleCloseModal} />
+
+      <GlobalStyle />
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from "../../assets/logo.svg";
+
+import { Container, Content } from "./styles";
+
+interface HeaderProps {
+  onOpenModal: () => void;
+}
+
+export function Header({ onOpenModal }: HeaderProps) {
+
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="dt money" />
+        <button type="button" onClick={onOpenModal}>Nova transação</button>
+      </Content>
+    </Container>
+  );
+}
